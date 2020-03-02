fcb94be53a335cf6beacfb5f6b54655d308bf272
juanagustin0504 <juanagustin@naver.com>
2020-03-02 17:55:21 +0900

    - add LoginViewModel

--- StudyToMG/Application/Main/Login/LoginViewModel.swift
+++ StudyToMG/Application/Main/Login/LoginViewModel.swift
@@ -9,5 +9,19 @@
 import Foundation
 
 class LoginViewModel {
+    var responseObj: LoginModel.Response? = nil
     
+    func requestLogin(USER_GB: String, USER_ID: String, USER_PW: String, APP_OS: String, APP_VER: String, INST_YN: String, completion: @escaping (NSError?) -> Void) {
+        let requestBody = LoginModel.Request(USER_GB: USER_GB, USER_ID: USER_ID, USER_PW: USER_PW, APP_OS: APP_OS, APP_VER: APP_VER, INST_YN: INST_YN)
+        
+        DataAccess.shared.fetch(api: "http://devsemo.wecontent.co.kr/SEMOGate.do?", requestBody: requestBody, responseType: LoginModel.Response.self) { (result) in
+            switch result {
+            case .failure(let error):
+                completion(error)
+            case .success(let responseObj):
+                self.responseObj = responseObj
+                completion(nil)
+            }
+        }
+    }
 }
