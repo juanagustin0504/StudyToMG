62d87ea169313efa98e287f5a4938fc297b30e03
juanagustin0504 <juanagustin@naver.com>
2020-03-02 16:45:09 +0900

    - replace MGModel

--- StudyToMG/Application/Main/MG/MGModel.swift
+++ StudyToMG/Application/Main/MG/MGModel.swift
@@ -10,39 +10,39 @@ import Foundation
 
 struct MGModel {
     
-    struct MGRequest: Encodable {
-        
-    }
+    struct MGRequest: Encodable { }
     
     struct MGResponse: Decodable {
-        let RESP_DATA                           : RES
+        let RESP_DATA: RES
         
         struct RES: Decodable {
-            let _tran_res_data                  : [TRAN_RES_DATA]
-            
+            let _tran_res_data: [TRAN_RES_DATA]
             
             struct TRAN_RES_DATA: Decodable {
-                let c_update_close              : Bool
-                let c_update_date               : String
-                let c_act_close                 : Bool
-                let c_forget_pw_url             : String
-                let _master_id                  : String
-                let c_register_url              : String
-                let c_appstore_url              : String
-                let _oid_list                   : [OID_LIST]
-                let c_session_time              : String
-                let _bank_list                  : [BANK_LIST]
-                let c_minimum_ver               : String
-                let c_act                       : String
-                let c_program_ver               : String
-                let c_available_service         : Bool
-                let _locale                     : String
-                let c_act_yn                    : Bool
-                let c_update_act                : String
-                let c_appstore_semotalk_url     : String
-                let c_site_url                  : String
-                let c_register_yn               : Bool
                 
+                let c_act_yn                    : Bool
+                let c_act_close                 : Bool
+                let c_register_yn               : Bool
+                let c_update_close              : Bool
+                let c_available_service         : Bool
+                
+                let _locale                     : String
+                let _master_id                  : String
+                
+                let c_act                       : String
+                let c_site_url                  : String
+                let c_update_act                : String
+                let c_minimum_ver               : String
+                let c_program_ver               : String
+                let c_update_date               : String
+                let c_appstore_url              : String
+                let c_register_url              : String
+                let c_session_time              : String
+                let c_forget_pw_url             : String
+                let c_appstore_semotalk_url     : String
+                
+                let _oid_list                   : [OID_LIST]
+                let _bank_list                  : [BANK_LIST]
                 
                 struct OID_LIST: Decodable {
                     let c_oid                   : String
@@ -52,10 +52,10 @@ struct MGModel {
                 
                 struct BANK_LIST: Decodable {
                     let c_bank_cd               : String
-                    let c_bank_scrap_cd         : String
-                    let c_display_order         : Int
                     let c_bank_name             : String
+                    let c_bank_scrap_cd         : String
                     let c_id_input_yn           : String
+                    let c_display_order         : Int
                 }
                 
             }
