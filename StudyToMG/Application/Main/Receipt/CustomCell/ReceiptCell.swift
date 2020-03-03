89a3954e9fa780c0e88e09887494f516f32d9bfc
juanagustin0504 <juanagustin@naver.com>
2020-03-03 18:50:52 +0900

    - add ReceiptViewController, tableView, customCell

---
+++ StudyToMG/Application/Main/Receipt/CustomCell/ReceiptCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceiptCell.swift
+//  StudyToMG
+//
+//  Created by Webcash on 2020/03/03.
+//  Copyright © 2020 Moonift. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
