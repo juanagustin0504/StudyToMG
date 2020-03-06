c8c13400674ed0b7f9125d81d9d95eab59c03933
juanagustin0504 <juanagustin@naver.com>
2020-03-06 15:44:45 +0900

    - add ReceiptCell

--- StudyToMG/Application/Main/Receipt/CustomCell/ReceiptCell.swift
+++ StudyToMG/Application/Main/Receipt/CustomCell/ReceiptCell.swift
@@ -9,16 +9,32 @@
 import UIKit
 
 class ReceiptCell: UITableViewCell {
+    
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var titleLb: UILabel!
+    @IBOutlet weak var dateLb: UILabel!
+    @IBOutlet weak var amountLb: UILabel!
+    
+    var dayOfWeek: String = String()
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        myViewStyle()
+        dateLb.text = dateLb.text! + "(\(dayOfWeek))"
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func myViewStyle() {
+        self.myView.layer.masksToBounds = true // 지정된 크기를 넘어가는 이미지 자르기
+        self.myView.layer.cornerRadius = 16 // 코너 둥글게
+        self.myView.layer.borderColor = UIColor.black.cgColor // 테두리 색
+        self.myView.layer.borderWidth = 1.0 // 테두리 굵기
     }
 
 }
--- StudyToMG/Application/Main/Receipt/ReceiptViewController.swift
+++ StudyToMG/Application/Main/Receipt/ReceiptViewController.swift
@@ -10,22 +10,33 @@ import UIKit
 
 class ReceiptViewController: UIViewController {
     
-    var receipt: [String:String] = [String:String]()
+    @IBOutlet weak var tableView: UITableView!
     
     let receiptViewModel: ReceiptViewModel = ReceiptViewModel()
+    var receiptObj : [SCMS_METC_R006.Response.REC]? = nil
+    
+    var curDate: Date = Date(timeIntervalSinceNow: 0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        receiptObj?.removeAll()
         receiptViewModel.request_SCMS_METC_R006(PAGE_NO: "0", PAGE_SZ: "10") { (error) in
             if error != nil {
                 print(":::::ERROR:::::")
                 print(error!)
             }
             print(":::::SUCCESS:::::")
-            print(self.receiptViewModel.responseObj ?? "nil")
-            print(":::::::::::::::::")
+            self.receiptObj = self.receiptViewModel.responseObj?.REC
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
+            print(self.curDate)
+            
         }
     }
     
@@ -33,5 +44,50 @@ class ReceiptViewController: UIViewController {
     @IBAction func backToMain(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
+    
+}
+
+//MARK: - delegate -
+extension ReceiptViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title = ""
+        if section == 0 {
+            title = "이번주"
+        } else if section == 1 {
+            
+        }
+        return title
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        }
+    }
+    
+}
+
+//MARK: - datasource -
+extension ReceiptViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ReceiptCell") as? ReceiptCell else {
+            return UITableViewCell()
+        }
+        cell.titleLb.text   = receiptObj?[indexPath.row].BZAQ_NM ?? "목포횟집"
+        cell.dateLb.text    = receiptObj?[indexPath.row].TRSC_DT ?? "5월 30일"
+        cell.amountLb.text  = receiptObj?[indexPath.row].TX_AMT ?? "-500,000"
+        cell.dayOfWeek      = receiptObj?[indexPath.row].TRSC_WEEK ?? "목"
+        
+        return cell
+    }
+    
     
 }
